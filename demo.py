b2deb0ae2ce046aad23fe9e46792d8bb3080bb48
abisheek <abisheekabhi@gmail.com>
2024-06-05 11:27:29 +0530

    message

---
+++ demo.py
@@ -0,0 +1,64 @@
+print('demo')
+print(10+5)
+print("ab"+"cd")
+print(10-2)
+print(10*6)
+print("asd"*5)
+print(15/2)
+print(10%3)
+print(10**8)
+print(19//3)
+print(10==10)
+print(10!=10)
+print(10!=11)
+print(10>5)
+print(10>11)
+print(10<5)
+print(10<11)
+print(10<=10)
+print(10<=11)
+print(10<=9)
+print(10>=10)
+print(10>=11)
+print(10>=9)
+a=10
+print(a)
+b=20
+b+=10
+print(b)
+c=10
+c-=20
+print(c)
+d=8
+print(10==10 and 10>8 and 10<9)
+print(10==10 and 10<9)
+print(10==10 or 10<10 or 10>5)
+print(not(True))
+a="synefoo"
+print('a' not in a)
+a=10
+b=10
+print(id(a))
+print(id(b))
+print(a is b)
+a=[10,20,30]
+b=[10,20,30]
+print((id(a)),(id(b)))
+print(a is b)
+print(a == b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
